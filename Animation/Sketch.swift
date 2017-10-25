2631be74e87a280338af2f58c509a15370fbc06f
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-10-25 10:18:39 -0400

    Rainbow Pong (Animations)

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var dx : Int
     // This function runs once
     override init() {
         
@@ -16,17 +16,33 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+        
+        // Set the difference for x
+        dx = 3
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        // Claer Background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
         // Change position
-        x += 1
+        x += dx
+        
+        // Check position and bounce back if touching edge
+        if x > 480 {
+           dx = -3
+        }
+        if x < 20 {
+            dx = 3
+        }
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.init(hue: x, saturation: 100, brightness: 100, alpha: 100)
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }
