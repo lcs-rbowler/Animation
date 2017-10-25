2e123024482f0f002bd9bf3601d843c628e1d245
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-10-25 10:31:08 -0400

    Five Circles (Animation)

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var x2 : Int
     
     // This function runs once
     override init() {
@@ -17,6 +18,7 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        x2 = 250
         
     }
     
@@ -25,10 +27,20 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
+        x2 -= 1
         
         // Draw an ellipse in the middle of the canvas
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: x2, centreY: 350, width: 50, height: 50)
+        canvas.fillColor = Color.green
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX: x2, centreY: 150, width: 50, height: 50)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
     }
     
 }
